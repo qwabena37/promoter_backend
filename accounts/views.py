6a026a9d5ea39b5c994f3ebda439589f3d570d3f
James Appiah <159532433+qwabena37@users.noreply.github.com>
2026-04-17 17:24:14 +0000

    add project files

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from rest_framework import generics
+from .models import User
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "email": request.user.email,
+            "expertise": request.user.expertise
+        })
